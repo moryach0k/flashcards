d3876307f3c354c581e8e27ca448880ea61176cb
moryach0k <kmorev17@gmail.com>
2016-04-02 15:03:37 +0300

    add validates

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,12 @@
 class Card < ActiveRecord::Base
+  validates :original_text, :translated_text, :review_date, presence: true
+  validate :original_and_translated_text_cannot_be_the_same
+
+  private
+  def original_and_translated_text_cannot_be_the_same
+    if original_text.casecmp(translated_text) == 0
+      errors.add(:original_text, "cant be the same")
+      errors.add(:translated_text, "cant be the same")
+    end
+  end
 end
