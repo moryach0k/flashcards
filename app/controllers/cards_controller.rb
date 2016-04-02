9b080ff7e23ba2ac35077232eb9941cda4068886
moryach0k <kmorev17@gmail.com>
2016-04-02 11:59:14 +0300

    add ability to create cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,10 +3,30 @@ class CardsController < ApplicationController
     @cards = Card.all
   end
 
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.review_date = Time.now.to_date + 3
+
+    if @card.save
+      redirect_to cards_path
+    else
+      render "new"
+    end
+  end
+
   def destroy
     @card = Card.find(params[:id])
     @card.destroy
 
     redirect_to cards_path
+  end
+
+  private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text)
   end
 end
