61a1d715d130b62d91d83de36aa72e45e3792654
moryach0k <kmorev17@gmail.com>
2016-04-02 10:55:56 +0300

    add destroy method for cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,11 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+
+    redirect_to cards_path
+  end
 end
