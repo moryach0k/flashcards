90a83498843ce90a4e5afa5f8f2c3bffb9e4156d
moryach0k <kmorev17@gmail.com>
2016-04-04 23:16:46 +0300

    edit seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,5 @@ rows = doc.search('//tbody//tr')
   ].each do |name, xpath|
     detail[name] = row.at_xpath(xpath).to_s.strip
   end
-  detail
   Card.create(original_text: detail[:original_text], translated_text: detail[:translated_text], review_date: Time.now.to_date)
 end
