d83858b9a53e4ce6310c69759b3b562d64d34aa9
moryach0k <kmorev17@gmail.com>
2016-04-04 22:37:26 +0300

    add seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'nokogiri'
+require 'open-uri'
+
+doc = Nokogiri::HTML(open('http://www.languagedaily.com/learn-german/vocabulary/common-german-words'))
+
+rows = doc.search('//tbody//tr')
+@details = rows.collect do |row|
+    detail = {}
+    [
+      [:original_text, 'td[2]/text()'],
+      [:translated_text, 'td[3]/text()'],
+    ].each do |name, xpath|
+      detail[name] = row.at_xpath(xpath).to_s.strip
+    end
+    detail
+end
+
+@details.each do |detail|
+  Card.create(original_text: detail[:original_text], translated_text: detail[:translated_text], review_date: Time.now.to_date)
+end
