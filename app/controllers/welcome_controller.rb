4efc7bee6650e308057854270c5a8e96244ca001
moryach0k <kmorev17@gmail.com>
2016-04-13 12:39:20 +0300

    update specs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @user = current_user
-    @card = @user.cards.needed_to_review.sample
+    unless @user.nil?
+      @card = @user.cards.needed_to_review.sample
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    email "MyString"
-    crypted_password "MyString"
-    salt "MyString"
+    email "user@email.com"
+    password "qwerty"
+    password_confirmation "qwerty"
   end
 end
