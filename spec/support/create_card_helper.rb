3673011b9527c5aa4d14c8e76ad5dce8e588e357
moryach0k <kmorev17@gmail.com>
2016-04-17 19:32:41 +0300

    edit create card feature

--- spec/support/create_card_helper.rb
+++ spec/support/create_card_helper.rb
@@ -3,6 +3,6 @@ def create_card(original_text, translated_text)
   click_link "add_card"
   fill_in "card_original_text", with: original_text
   fill_in "card_translated_text", with: translated_text
-  page.attach_file("card_image", Rails.root + "public/system/cards/images/img.jpg")
+  page.attach_file("card_image", "public/system/cards/images/img.jpg")
   click_button "add_card_btn"
 end
