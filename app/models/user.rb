b4904ddcd4737a138e9c8d92a3d0ede0cde4d254
moryach0k <kmorev17@gmail.com>
2016-04-26 10:34:41 +0300

    add schedule for emails about pending cards

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,11 @@ class User < ActiveRecord::Base
 
   has_many :cards
   has_many :decks
+
+  def notify_about_pending_cards
+    User.all.each do |user|
+      unless user.cards.needed_to_review.nil?
+        CardsMailer.pending_cards_notification(user).deliver_now
+    end
+  end
 end
